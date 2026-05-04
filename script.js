a4bb7165d68a74f628c5a399b1c34f0a5c81aab2
RaulMNZ <40411@raphaeldisanto.com.br>
2026-05-04 07:55:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let frases = [
     "Nunca subestime o poder do trabalho em equipe - Halo 3",
     "O aço ganha as batalhas, o ouro ganha a guerra - Davion Dota 2",
     "Existe diferença entre sobreviver e viver - The Last of Us",
-    "As vezes é melhor ter paciência do que agir impulsivamente - Zelda BOTW"
-    "Goool - EAFC 2026"
-    "Não importa o quão escuro seja a noite, o amanhã sempre chega - Final Fantasy X"
-    "Espaço. Espaço. Eu to no ESPAAAAAÇO - Portal 2"
+    "As vezes é melhor ter paciência do que agir impulsivamente - Zelda BOTW",
+    "Goool - EAFC 2026",
+    "Não importa o quão escuro seja a noite, o amanhã sempre chega - Final Fantasy X",
+    "Espaço. Espaço. Eu to no ESPAAAAAÇO - Portal 2",
     "Get over here! - Mortal Kombat"
 ];
 
@@ -14,9 +14,9 @@ let cliques = 0;
 function gerarFrases(){
     let indice = Math.floor(Math.random() * frases.length);
 
-    let fraseEscolhida = frases[indice]:
+    let fraseEscolhida = frases[indice];
 
-    clique = cliques+1;
+    cliques = cliques+1;
 
     document.getElementById("frase").innerText = fraseEscolhida;
 
